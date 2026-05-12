b16ae3f93ecca90ebd6d2d214e6d38343e20f484
Angus2510 <124569463+Angus2510@users.noreply.github.com>
2026-05-12 10:40:47 +0200

    Add ADI registration plugin

---
+++ plugins/withAdiRegistration.js
@@ -0,0 +1,22 @@
+const { withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+const withAdiRegistration = (config) => {
+  return withDangerousMod(config, [
+    'android',
+    async (config) => {
+      const assetsDir = path.join(config.modRequest.platformProjectRoot, 'app', 'src', 'main', 'assets');
+      if (!fs.existsSync(assetsDir)) {
+        fs.mkdirSync(assetsDir, { recursive: true });
+      }
+      fs.writeFileSync(
+        path.join(assetsDir, 'adi-registration.properties'),
+        'C2TYWBDI5DGDKAAAAAAAAAAAAA'
+      );
+      return config;
+    },
+  ]);
+};
+
+module.exports = withAdiRegistration;
